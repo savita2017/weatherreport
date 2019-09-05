de23a67d6e0f8e6a474361f236285a945f342810
SmartCoding <savita20khurana17@gmail.com>
2019-09-05 23:50:07 +0200

    weather forecast of city of particular country

---
+++ src/components/titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title-container__title">Weather Forecast App </h1>
+                <p className="title-container__subtitle"> Update you about Weather Conditions of any City </p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
